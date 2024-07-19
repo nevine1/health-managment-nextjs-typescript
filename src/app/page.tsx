3442bddecfcb2a0bbb870861134ab36ae5089bc2
nevine1 <venasamaan@gmail.com>
2024-07-18 22:59:14 -0400

    still working on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,31 @@
 import Image from "next/image";
-
+import Logo from '../../public/assets/icons/logo-full.svg'
+import OnBoarding from '../../public/assets/imgs/onboarding-img.png'
+import PatientForm from '../components/forms/PatientForm'
+import Link  from "next/link";
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-white text-[90px] underline">Hello nexts </h1>
+    <div className="flex h-screen max-h-screen ">
+      <section className="remove-scrollbar container my-auto">
+        <div className="max-w-[496px] flex flex-col mx-auto py-10 size-full">
+          <Image src={Logo} alt="patient" width={1000} height={1000} className="w-fit mb-12 h-10"/>
+          <PatientForm/>
+          <div className="text-14-regular flex flex-between mt-20">
+            <p className="justify-items-end xl:text-left text-dark-600">
+              &copy; 2024 CarePulse
+            </p>
+            <Link  href="/?admin=true"
+              className="text-green-500 cursor-pointer"
+              >
+              Admin
+            </Link>
+          </div>
+        </div>
+      </section>
+      <Image src={OnBoarding} alt="Patient"
+        width={1000} height={1000}
+        className="max-width-[50%]  hidden h-full object-cover md:block"
+      />
     </div>
   );
 }
---
+++ src/components/forms/PatientForm.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const PatientForm = (props: Props) => {
+  return (
+    <div>
+        <h2>Helloooooooooooooooooooooooooo</h2>
+    </div>
+  )
+}
+
+export default  PatientForm;
